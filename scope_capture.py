5203cdb12ba5604b527ea6fc281ea27016a1514d
snowfall! <155919995+nosnowfall@users.noreply.github.com>
2025-04-10 17:52:17 -0500

    Update scope_capture.py
    some minor bug fixes and style updates

--- scope_capture.py
+++ scope_capture.py
@@ -1,12 +1,15 @@
 """the aim of this script is to automate screen captures from Teledyne Lecroy Oscilloscopes running Maui
-Created 2024-09-19 by Elijah Berger (@nosnowfall)
+Created 2024-09-19 (@nosnowfall)
+Modified 2025-04-10 (@nosnowfall)
 License: use as you wish, but please credit me for derivations, forks, etc.
 Documentation: see the docs for tkinter, pyvisa, and Teledyne Lecroy's Remote/Automation manuals
 
 Technical disclaimers:
 You must first install pyvisa (from pip), and then the appropriate NI VISA drivers from National Instruments
 This script relies upon configurations in the same directory, scopecaptureconfig.ini
-It assumes a Windows x64 machine, but can be easily modified for x32 or Unix"""
+It assumes a Windows x64 machine, but can be easily modified for x32 or Unix
+USB based connections are wayyy more stable than TCP/IP"""
+
 import logging
 import configparser
 from tkinter import *
@@ -14,6 +17,49 @@ from tkinter import filedialog
 from tkinter import ttk
 from pathlib import Path
 import pyvisa
+from datetime import datetime
+
+class Oscope():
+    """ Wrapping the oscilloscope connection in a class solves some tkinter issues """
+    def __init__(self) -> None:
+        self.visaobj :pyvisa.resources.Resource = 0 # dummy val
+
+    def update_oscope(self, addr: pyvisa.resources.Resource) -> None:
+        """ change the target VISA address """
+        self.visaobj = addr
+        logger.info(f'changed addr to: {addr}')
+
+class RM_Manager():
+    def __init__(self) -> None:
+        self.resman = 0 # dummy val
+        self.reslist :list = [] # fill after opening connection
+        self.start_resman() # true init function
+
+    def start_resman(self) -> None:
+        """ Starts the PyVISA resource manager instance and grabs a list of available VISA resources """
+        logger.info('starting VISA manager')
+        self.resman :pyvisa.ResourceManager = pyvisa.ResourceManager("C:\\Windows\\System32\\visa64.dll")
+        self.reslist = self.resman.list_resources() # this will find any saved resources in NI Max unfortunately
+        logger.debug(f'RM found VISA resources: {self.reslist}')
+
+    def reset_resman(self) -> None:
+        """ Reset the resource manager in case of error """
+        logger.info('resetting VISA manager')
+        self.reslist.clear()
+        self.close_links()
+        self.close_resman()
+        self.start_resman()
+    
+    def close_links(self) -> None:
+        """ Close any active VISA resource """
+        logger.info('clearing VISA links')
+        for object in self.resman.list_opened_resources():
+            object.close()
+
+    def close_resman(self) -> None:
+        """ Ensure the PyVISA backend closes """
+        logger.info('closing resource manager')
+        self.resman.close()
 
 def initial_config() -> tuple[configparser.ConfigParser, Path]:
     """load or create if not found settings for the script
@@ -22,35 +68,36 @@ def initial_config() -> tuple[configparser.ConfigParser, Path]:
     imagepath | default save directory, does NOT check validity at runtime
     imagename | default filename, to be replaced with autogeneration
     instrumentaddr | for faster connections to the same machine"""
-    logging.info('loading configuration files...')
-    config = configparser.ConfigParser()
-    configfilepath = Path(__file__).parent / 'scopecaptureconfig.ini'
-    logging.debug(f'looking for: {configfilepath}')
+    logger.info('loading configuration files...')
+    config :configparser.ConfigParser = configparser.ConfigParser()
+    configfilepath :Path = Path(__file__).parent / 'scopecaptureconfig.ini'
+    logger.debug(f'looking for: {configfilepath}')
     if not config.read(configfilepath): # returns false if the file is nonexistant or empty
-        logging.debug('could not find scopecaptureconfig.ini; creating it now...')
-        config['config'] = {'background': 'WHITE', 'imagepath': 'C:\\Users\\Public\\Pictures', 'imagename': 'screencapture.jpeg', 'instrumentaddr': 'USB0::TEMPLATE'}
+        logger.warning('could not find scopecaptureconfig.ini; creating it now...')
+        config['config'] = {'background': 'WHITE', 'imagepath': 'C:\\Users\\Public\\Pictures'}
         save_config(config, configfilepath)
     else:
-        logging.debug('found scopecaptureconfig.ini...')
+        logger.debug('found scopecaptureconfig.ini...')
     for key in config['config']:
-        logging.info(f'set {key}: {config['config'][key]}')
+        logger.info(f'set {key}: {config['config'][key]}')
     return config, configfilepath
 
 def save_config(config: configparser.ConfigParser, filepath: Path) -> None:
-    """helper function so users can change configs later"""
-    logging.info('saving updated configuration')
+    """ helper function for saving user-changed config """
+    logger.info('saving updated configuration')
     config.write(open(filepath,'w'))
     return None
 
 def change_config(config: configparser.ConfigParser, key: str, val: str) -> None:
-    logging.debug(f'changing config of {key} to {val}')
+    """ helper function for changing config without saving """
+    logger.debug(f'changing config of {key} to {val}')
     config['config'][key] = val
     return None
 
-def main():
+def main() -> None:
     cfg, cfgpath = initial_config() # tkinter is often used with global scope, but we are trying to avoid that
 
-    # main window for tkinter
+    # main window for tkinter, todo make resizeable
     root = Tk()
     root.title("Oscilloscope Screen Capture")
     main = ttk.Frame(root, padding="3 3 12 12")
@@ -59,48 +106,25 @@ def main():
     root.rowconfigure(0, weight=1)
 
     # connection settings
-    visastatustext = StringVar() # displays status of visa resourcemanager
-    visastatustext.set('VISA: DOWN')
-    visastatus = BooleanVar() # todo: use for button enable/disable
-    visastatus.set(False)
-
     connstatustext = StringVar() # displays status of instrument connection
     connstatustext.set('LINK: DOWN')
     connstatus = BooleanVar() # todo: use for button enable/disable
     connstatus.set(False)
     
-    oscope = pyvisa.Resource # i think this hack allows callbacks to edit the variable, circumventing stupid macro chains
+    resman = RM_Manager() # object wrapper hack part two
 
-    def loadvisa() -> tuple[pyvisa.ResourceManager, list]:
-        """should not fail macro to create a Resource Manager, called on startup"""
-        logging.debug('loading VISA resource manager')
-        try:
-            rm = pyvisa.ResourceManager("C:\\Windows\\System32\\visa64.dll") # you should specify the dll, see pyvisa docs for more info
-            resources = rm.list_resources()
-        except Exception as e: # prevent crashes, just let us try again later
-            logging.warning(f'Visa resource manager crashed: {repr(e)}')
-            visastatus.set(False)
-            visastatustext.set('VISA: DOWN')
-            rm = None
-            resources = []
-        else:
-            visastatus.set(True)
-            visastatustext.set("VISA: UP")
-            logging.debug('VISA RM loaded successfully')
-        finally:
-            return rm, resources
+    oscope = Oscope() # trying a new object wrapper hack to fool tkinter
     
     def tryconnect() -> None:
-        """try to open visa comms with instrument, fails quite often for I think backend bug reasons"""
-        logging.debug(f'trying connection to {cfg['config']['instrumentaddr']}')
-        for addr in rm.list_opened_resources(): # prevent duplicate open resources
-            addr.close()
-            connstatustext.set('LINK: DOWN')
+        """ try to open visa comms with instrument, fails quite often for I think backend bug reasons """
+        logger.debug(f'trying connection to {target.get()}')
+        resman.close_links() # make sure we don't duplicate links
+        connstatustext.set('LINK: DOWN')
         try:
-            oscope = rm.open_resource(cfg['config']['instrumentaddr']) # pull from cfg for callback ability
+            oscope.update_oscope(resman.resman.open_resource(target.get())) # pull from cfg for callback ability
         except Exception as e:
-            logging.warning(f'Instrument connection failed: {repr(e)}')
-            oscope = pyvisa.Resource # same hack again, fingers crossed
+            logger.warning(f'Instrument connection failed: {repr(e)}')
+            oscope.update_oscope(0)
             connstatus.set(False)
             connstatustext.set('LINK: DOWN')
         else:
@@ -108,36 +132,30 @@ def main():
             connstatustext.set('LINK: UP')
         finally:
             return None
-    
-    def settarget() -> None:
-        """callback macro to change instrument address in cfg
-        could probably be a lambda at button definition"""
-        change_config(cfg, 'instrumentaddr', target.get())
 
-    rm, resources = loadvisa() # i couldnt find a way to initialize these after the window is created
-
-    visaframe = ttk.Labelframe(main, text='NI Visa Status')
-    visaframe.grid(column=0,row=0)
-    visastatuslabel = ttk.Label(visaframe, textvariable=visastatustext)
-    visastatuslabel.grid(column=0,row=0)
-    visabutton = ttk.Button(visaframe, text='Try VISA', command=loadvisa) # this won't work as is, because we need to return the RM and resources
-    visabutton.grid(column=0,row=1)
-
+    def retryvisa() -> None:
+        """ reset the Resource Manager and refresh menu of VISA devices """
+        resman.reset_resman()
+        connentry['values'] = resman.reslist
+    # retry button
+    visabutton = ttk.Button(main, text='Retry VISA', command=retryvisa) # this won't work as is, because we need to return the RM and resources - 2025-04-10 maybe not an issue?
+    visabutton.grid(column=0,row=0)
+    # status and connect box
     connframe = ttk.Labelframe(main, text='Instrument Status')
-    connframe.grid(column=1, row=0)
+    connframe.grid(column=1, row=0, columnspan=2, sticky=EW)
     connstatuslabel = ttk.Label(connframe, textvariable=connstatustext)
     connstatuslabel.grid(column=0,row=0)
     connbutton = ttk.Button(connframe, text='Connect Instrument', command=tryconnect)
     connbutton.grid(column=1, row=0)
-    
+    # let user choose target
     target = StringVar()
-    target.set(cfg['config']['instrumentaddr'])
-    connentry = ttk.OptionMenu(connframe, variable=target, *resources, command=settarget) # autopopulates from resources i think
+    connentry = ttk.Combobox(connframe, width=45, textvariable=target) # can we make size variable?
+    connentry['values'] = resman.reslist # autopopulates from resources i think
     connentry.grid(column=0,row=1,columnspan=2)
 
     # background color, radiobutton choice and saves to cfg
     bckgframe = ttk.LabelFrame(main, text='Background color')
-    bckgframe.grid(column=2, row=3, sticky=EW)
+    bckgframe.grid(column=0, row=4, sticky=EW)
     bckg = StringVar()
     bckg.set(cfg['config']['background'])
     black = ttk.Radiobutton(bckgframe, text='Black', variable=bckg, value='BLACK', command=lambda: change_config(cfg, 'background', 'BLACK'))
@@ -147,59 +165,93 @@ def main():
 
     # image save directory, using file picker dialog box
     def choose_savedir() -> None:
-        newdir = filedialog.askdirectory()
+        """ open file dialog to pick a savefile directory - remembers from last use """
+        newdir = filedialog.askdirectory() # there is a bug with root not closing filedialogs but only with many many opens.
+        # ^^ beware of unavailable network drives, Windows 11 crashes when some programs try to read them
         imagepath.set(newdir)
         change_config(cfg, 'imagepath', newdir)
     ttk.Label(main, text='Save to:').grid(column=0, row=1, sticky=E)
 
     imagepath = StringVar()
     imagepath.set(cfg['config']['imagepath'])
-    imagepath_entry = ttk.Label(main, textvariable=imagepath, background='#d3d3d3') # need to update functionality - currently doesnt change cfg
+    imagepath_entry = ttk.Label(main, textvariable=imagepath, background='#d3d3d3') # need to update functionality - currently doesnt change cfg - 2025-04-10 maybe it does now
     imagepath_entry.grid(column=1, row=1, sticky=EW)
     browsebutton = ttk.Button(main, text='Browse', command=choose_savedir)
     browsebutton.grid(column=2, row=1, sticky=W)
 
     # screencap
     def prtscrmacro() -> None:
+        """ sends VISA command to print screen, reads response, calls savemacro()
+        
+        If a new kind of scope needs to be added, this is the place to start tweaking """
         hcsucmd = f"HCSU DEV, JPEG, BCKG, {cfg['config']['background']}, AREA, GRIDAREAONLY, PORT, NET" # setup screen capture params
-        oscope.write(hcsucmd)
-        oscope.write('SCDP') # ask scope to make a screen capture, which according to our previous command will send over VISA
-        capture = oscope.read_raw() # read image file from instrument
+        oscope.visaobj.write(hcsucmd)
+        oscope.visaobj.write('SCDP') # ask scope to make a screen capture, which according to our previous command will send over VISA
+        capture = oscope.visaobj.read_raw() # read image file from instrument
         savemacro(capture)
 
     def savemacro(photo: bytes) -> None:
+        """ saves data to file as jpeg """
+        if autonaming.get():
+            filename = datetime.now().strftime('scopecapture_%Y%m%d_%H%M%S%f')[:-3] + '.jpeg'
+        else:
+            filename = imagename.get()
         savedir = Path(cfg['config']['imagepath'])
-        f = open(savedir / imagename.get(), 'wb+') # wb+ for binary writing. todo: autogenerate names
+        logger.info(f'writing to file now')
+        f = open(savedir / filename, 'wb+') # wb+ for binary writing. todo: auto increment names
         f.write(photo)
         f.close()
 
-    ttk.Label(main, text='Save as:').grid(column=0, row=3, sticky=E)
+    # choosing filename
+    def namebuttonfunc():
+        """ callback to enable or disable automatic file naming """
+        if autonaming.get():
+            imagename_entry.config(state='disabled')
+        else:
+            imagename_entry.config(state='enabled')
+
+    nameframe = ttk.LabelFrame(main, text='Save as:')
+    nameframe.grid(column=1, row=3, columnspan=2, sticky=EW)
     imagename = StringVar()
+    autonaming = BooleanVar()
+    autonaming.set(True)
     imagename.set(cfg['config']['imagename'])
-    imagename_entry  = ttk.Entry(main, textvariable=imagename, background='#d3d3d3')
-    imagename_entry.grid(column=1, row=3, sticky=EW)
+
+    autonameframe = ttk.LabelFrame(main, text='Naming')
+    autonameframe.grid(column=0, row=3, sticky=EW)
+    auto = ttk.Radiobutton(autonameframe, text='Auto', variable=autonaming, value=True, command=namebuttonfunc)
+    manual = ttk.Radiobutton(autonameframe, text='Manual', variable=autonaming, value=False, command=namebuttonfunc)
+    auto.pack(side=LEFT)
+    manual.pack(side=RIGHT)
+
+    imagename_entry  = ttk.Entry(nameframe, textvariable=imagename, background='#d3d3d3', state='disabled')
+    imagename_entry.pack()
+    
     screengrabber = ttk.Button(main, text='Print Screen', command=prtscrmacro)
     screengrabber.grid(column=1, row=4, sticky=EW)
 
     for child in main.winfo_children():
         child.grid_configure(padx=5, pady=5)
 
-    root.mainloop()
+    root.mainloop() #  party starts now
 
     # cleanup
-    for addr in rm.list_opened_resources():
-        addr.close()
-    rm.close()
+    resman.close_links()
+    resman.close_resman()
+
+    cfg['config']['instrumentaddr'] = 'No instrument found' # prevent false information on next start
     save_config(cfg, cfgpath)
     return None
 
 if __name__ == "__main__":
     logging.basicConfig(
-        level = logging.DEBUG,
+        level = logging.INFO,
         format = '%(asctime)s.%(msecs)03d %(levelname)s: %(message)s',
         datefmt = '%Y-%m-%d %H:%M:%S',
         encoding = 'utf-8'
     )
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
     logging.debug('Logging initialized')
     try:
         main()
